f54cd4f07f832204352c2becf1130be356a9b384
David Benson <david@jgraph.com>
2017-04-11 23:00:46 +0100

    Navigates sub-folders to convert .vsdx files

--- src/vsdxBatchConvert.java
+++ src/vsdxBatchConvert.java
@@ -1,10 +1,11 @@
 
 import java.io.File;
-import java.io.FilenameFilter;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
-import java.util.Arrays;
+import java.nio.file.Path;
+import java.util.stream.Stream;
 
 import javax.swing.JFileChooser;
 import javax.xml.parsers.ParserConfigurationException;
@@ -45,56 +46,80 @@ public class vsdxBatchConvert
 	{
 		System.out.println(folder);
 		
-		File[] files = folder.listFiles(new FilenameFilter()
-		{ 
-            public boolean accept(File dir, String filename)
-            {
-            	return filename.endsWith(".vsdx");
-            }
-		} );
-		
-		System.out.println(Arrays.toString(files));
-		
-		for (int i = 0; i < files.length; i++)
+		try (Stream<Path> filePathStream=Files.walk(folder.toPath()))
 		{
-			File file = files[i];
-			byte [] fileData = Files.readAllBytes(file.toPath());
-			
-			if (fileData != null)
-			{
-				mxVsdxCodec vdxCodec = new mxVsdxCodec();
-				String xml = null;
+		    filePathStream.forEach(filePath ->
+		    {
+		        if (Files.isRegularFile(filePath))
+		        {
+		        	if (filePath.toString().endsWith(".vsdx"))
+		        	{
+		    			byte[] fileData = null;
 
-				try
-				{
-					xml = vdxCodec.decodeVsdx(fileData, Utils.CHARSET_FOR_URL_ENCODING);
-				}
-				catch (ParserConfigurationException e)
-				{
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				catch (SAXException e)
-				{
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				catch (TransformerException e)
-				{
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				
-				if (xml != null)
-				{
-					String path = StringUtils.substringBefore(file.getPath(), ".");
+						try
+						{
+							fileData = Files.readAllBytes(filePath);
+						}
+						catch (IOException e1)
+						{
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+		    			
+		    			if (fileData != null)
+		    			{
+		    				mxVsdxCodec vdxCodec = new mxVsdxCodec();
+		    				String xml = null;
 
-					try (PrintWriter out = new PrintWriter(path + ".xml"))
-					{
-					    out.println(xml);
-					}
-				}
-			}
+		    				try
+		    				{
+		    					xml = vdxCodec.decodeVsdx(fileData, Utils.CHARSET_FOR_URL_ENCODING);
+		    				}
+		    				catch (ParserConfigurationException e)
+		    				{
+		    					// TODO Auto-generated catch block
+		    					e.printStackTrace();
+		    				}
+		    				catch (SAXException e)
+		    				{
+		    					// TODO Auto-generated catch block
+		    					e.printStackTrace();
+		    				}
+		    				catch (TransformerException e)
+		    				{
+		    					// TODO Auto-generated catch block
+		    					e.printStackTrace();
+		    				}
+							catch (IOException e)
+							{
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+		    				
+		    				if (xml != null)
+		    				{
+		    					String path = StringUtils.substringBefore(filePath.toString(), ".");
+
+		    					try (PrintWriter out = new PrintWriter(path + ".xml"))
+		    					{
+		    					    out.println(xml);
+		    					}
+								catch (FileNotFoundException e)
+								{
+									// TODO Auto-generated catch block
+									e.printStackTrace();
+								}
+		    				}
+		    			}
+
+		        	}
+		        }
+		    });
+		}
+		catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 
@@ -104,7 +129,6 @@ public class vsdxBatchConvert
 	public static File selectFile(String title, String extension)
 	{
 		JFileChooser chooser = new JFileChooser();
-		// chooser.addChoosableFileFilter(new FileNameExtensionFilter(extension.toUpperCase() + " File", extension));
 		chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 		chooser.setDialogTitle(title);
 
